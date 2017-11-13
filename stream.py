7a6f23850d12eabaebdbbca910ae4a5261e43310
Jacob Mintzer <jrmintz3@gmail.com>
2017-11-13 17:06:11 -0500

    program now collects tweet IDs real time, and then submits them after an hour, so that the data can actually have favorites, retweets etc. Unfortunately the moment a tweet is posted, it does not have any interactions, so the previous version unfortunately did not work. Debug mode was also added for more immediate data analysis, and to not post onto twitter when the program isn't working right.

--- stream.py
+++ stream.py
@@ -26,6 +26,8 @@ class Statistics:
 		self.startTime = time.time()
 		self.totalEntries=0	
 		self.popularityIndex=[]
+		self.tweetIDs=[]
+		self.times=[]
 		if clean:
 			for tag in tags:
 				self.popularityIndex.append(0)
@@ -33,15 +35,26 @@ class Statistics:
 			with open("popularity.txt") as pop:
 				popList=pop.readline().split()
 			for popVal in popList:
+				print(str(popVal))
 				self.popularityIndex.append(int(popVal))
 		for tag in tags:
 			self.stored_data.append({})
+			print (tag)
 			self.labels.append(tag)
 
 	def clear(self):
 		self.stored_data=[]
 		for tag in self.labels:
 			self.stored_data.append({})
+
+	def collect(self,ID):
+		if(len(self.tweetIDs)==0):
+			self.tweetIDs.append([])
+			self.times.append(time.localtime())
+		elif len(self.tweetIDs[len(self.tweetIDs)-1])%100==0:
+			self.tweetIDs.append([])
+			self.times.append(time.localtime())
+		self.tweetIDs[len(self.tweetIDs)-1].append(ID)
 
 	def add(self,data):
 		try:
@@ -52,6 +65,7 @@ class Statistics:
 					if tag in data["text"]:
 						found=True
 				if found:
+					print("found")
 					if data["user"]["id_str"] in page.keys():
 						page[data["user"]["id_str"]].append([data["quote_count"],data["reply_count"],data["retweet_count"],data["favorite_count"]])
 						found=False
@@ -71,7 +85,7 @@ class Statistics:
 	def export(self):
 		with open("popularity.txt",'w+') as file:
 			for popVal in self.popularityIndex:
-				file.write(popVal)
+				file.write(str(popVal))
 				file.write(" ")
 
 
@@ -94,7 +108,8 @@ class StdOutListener(StreamListener):
 
 	def on_data(self, data):
 		global tweetData
-		tweetData.add(json.loads(data))
+		ID=json.loads(data)
+		tweetData.collect(ID["user"]["id_str"])
 		return True
 
 	def on_error(self, status):
@@ -116,28 +131,32 @@ def analyze(auth,Status):
 	popularityIndex=tweetData.popularityIndex
 	totalTweeters=[]
 	mainTopics=[]
+	TweetIDs=[]
+	if Status=="debug":
+		debug=True
+	else:
+		time.sleep(3700)
 	while True:
 		try:
 			
 
 			while(True):	#system so that program only runs at 6 or 12, adjusted since server is an hour off of my time
 				curTime=time.localtime()
-				if(curTime.tm_hour==23 or curTime.tm_hour==5 or curTime.tm_hour==11 or curTime.tm_hour==17):
-					if(curTime.tm_min>5):
+				if debug:
+					time.sleep(30)
+					break
+				if curTime.tm_hour==23 or curTime.tm_hour==5 or curTime.tm_hour==11 or curTime.tm_hour==17:
+					if curTime.tm_min>5:
 						time.sleep(5*60*(60-curTime.tm_min))
 					else:
 						break
 				else:
 					time.sleep(298)
 			tweetData.lock.acquire()
-			# tweetData.stored_data=[{"string":7},{"string2":8}]
-			# for topicData in tweetData.stored_data:
-			# 	print(type(topicData))
-			# 	for user in topicData:
-			# 		print(type(topicData[user]))
+			
 
 			#compiling data from each topic
-			for label,topicData,popularity,totTweet in zip(tweetData.labels,tweetData.stored_data,popularityIndex,totalTweets):
+			for label,topicData,popularity in zip(tweetData.labels,tweetData.stored_data,popularityIndex):
 				mainTopics.append(label[0])
 				numTweets=0
 				Tweeters=0
@@ -165,41 +184,68 @@ def analyze(auth,Status):
 						popPerTweet=fav**(1/4)+retweet**(1/2)+reply**(1/3)+quote**(1/3)
 						userPop+=popPerTweet
 					popularity+=userPop**(1/2)
-				totTweets+=numTweets
+				
 				#popularityIndex.append(popularity)
 				totalTweeters.append(Tweeters)
 			tweetData.export()
 			tweetData.lock.release()
 			#trace=go.Bar(x=tweetData.labels,y=popularityIndex)
+			#print (mainTopics)
+			#print (popularityIndex)
 			data = [go.Bar(
             x=mainTopics,
             y=popularityIndex)]
 
-			layout=go.Layout(title='{}'.format(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:00')),width=1600, height=900)
+			layout=go.Layout(title='{}'.format(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M')),width=1600, height=900)
 			fig = go.Figure(data=data, layout=layout)
 			py.image.save_as(fig, filename='image.png')
 			api=tweepy.API(auth)
-			if Status=="":
+			if debug:
+				print("not posting to twitter because in debug mode, check the file yourself")
+			elif Status=="":
 				api.update_with_media(filename='image.png',status="{}".format(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:00')))
 			else:
-				api.update_with_media(filename='image.png',status=Status+"\n{}".format(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:00')))
+				api.update_with_media(filename='image.png',status="{1}\n{0}".format(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:00'),Status))
 				Status=""
-			#print("posted , sleeping for 6 hours")
-			time.sleep(300)
+			print("posted")
+			if not debug:
+				time.sleep(300)
 		except Exception as ex:
 			print("{} occured in analysis, resetting\n".format(ex))
 			print(tweetData.labels)
 			#tweetData.lock.release()
 		finally:
-			for pop in popularityIndex:
+			tweetData.popularityIndex=popularityIndex
+			for pop in tweetData.popularityIndex:
 				pop=pop**.75
 
 def stream(mystream,megatag):
 	while True:
 		try:
 			myStream.filter(track=megatag)
-		except AttributeError:
-			print("error in stream at {}, resetting stream\n".format(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')))
+		except Exception as error:
+			print("error {1} in stream at {0}, resetting stream\n".format(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'),error))
+
+def compile(auth):
+	global tweetData
+	time.sleep(3600)
+	api=tweepy(auth)
+	while True:
+		try:
+			curTime=time.localtime()
+			if (tweetData.times[0].tm_hour<curTime.tm_hour or (tweetData.times[0].tm_hour==23 and curTime.tm_hour<=1 ) and tweetData.times[0].tm_min<curTime.tm_min) or (tweetData.times[0].tm_hour+2)%24<=curTime.tm_hour:
+				dataList=api.statuses_lookup(tweetData.tweetIDs.pop(0))
+				tweetData.times.pop(0)
+				for data in dataList:
+					tweetData.add(data)
+				print (len(tweetData.times))
+				time.sleep(60)
+			else:
+				time.sleep (120)
+		except error:
+			print ("{} error in compilation".format(error))
+
+
 
 # def fileCheck(fileName):
 # 	fileDir=os.path.dirname(os.path.realpath('__file__'))
@@ -227,15 +273,20 @@ if __name__=='__main__':
 	elif sys.argv[1]=="1":
 		Status="Program just crashed, restarting with previous data on {}.".format(datetime.datetime.fromtimestamp(time.time()).strftime('%m-%d at %H:%M'))
 		clean=False
-	else:
+	elif sys.argv[1]=="2":
 		Status="Program stopped at unknown time, restarting with previous data on {}".format(datetime.datetime.fromtimestamp(time.time()).strftime('%m-%d at %H:%M'))
 		clean=False
+	else:
+		clean=False
+		Status="debug"
+		print ("\n\nWARNING, YOU ARE IN DEBUG MODE, THIS ISN'T ACTUALLY RUNNING, IT WILL NOT BE POSTED TO TWITTER\n\n")
 	tweetData=Statistics(queryData["tags"],clean)
 	myStreamListener = StdOutListener()
 	myStream = Stream(auth, myStreamListener)
 	_thread.start_new_thread ( stream, (myStream,megatag) )
-	print (megatag)
-	analyze(auth,Status) 
+	#print (megatag)
+	_thread.start_new_thread(analyze, (auth,Status) )
+	compile(auth)
 	# while True:
 	# 	time.sleep(120)
 		
